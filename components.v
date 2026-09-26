b45edfd188398f08e7b613f09fb26f4d6c13ac84
sjos0058 <sjos0058@student.monash.edu>
2026-09-26 11:01:00 +1000

    Add files via upload

--- components.v
+++ components.v
@@ -7,71 +7,65 @@ Please enter your name and student ID:
 
 */
 module sign_extend(in, ext);
-	/* 
-	 * This module sign extends the 9-bit Din to a 16-bit output.
-	 */
-	// TODO: Declare inputs and outputs
-	
-	// TODO: implement logic
+	input [8:0] in;
+	output [15:0] ext;
+
+	assign ext = {{7{in[8]}}, in};
 endmodule
 
 
 
 module tick_FSM(rst, clk, enable, tick);
-	/* 
-	 * This module implements a tick FSM that will be used to
-	 * control the actions of the control unit
-	 */
+	module tick_FSM(tick, enable, reset, clock);
+    input enable;
+    input reset;
+    input clock;
+    output reg [3:0] tick;
 
-	// TODO: Declare inputs and outputs
-	
-    // TODO: implement FSM
-endmodule
-
-module multiplexer(SignExtDin, R0, R1, R2, R3, R4, R5, R6, R7, G, sel, Bus);
-    input wire [15:0] R0; //registor 0 
-    input wire [15:0] R1; //registor 1
-    input wire [15:0] R2; //..
-	 input wire [15:0] R3;
-	 input wire [15:0] R4;
-	 input wire [15:0] R5;
-	 input wire [15:0] R6;
-	 input wire [15:0] R7;//registor 7
-	 input wire [15:0] G; //registor G
-	 input wire [15:0] SignExtDIN; //sign extended DIN
-	 input wire [3:0]  sel;// selection
-    output reg  [15:0] result; //output
-
-
-    always @(*) begin
-        case (sel)
-            4'd0  :  result = A0;
-            4'd1  :  result = A1;
-            4'd2  :  result = A2;
-            4'd3  :  result = A3;
-				4'd4  :  result = A4;
-				4'd5  :  result = A5;
-				4'd6  :  result = A6;
-				4'd7  :  result = A7;
-				4'd8  :  result = G;
-				4'd9  :  result = SignExtDin;
-				default : result = 16'hXXXX;
-        endcase
+    always @(posedge clock) begin
+        if (reset) begin
+            tick <= 4'b0001;
+        end
+        else if (enable) begin
+            case (tick)
+                4'b0001: tick <= 4'b0010;
+                4'b0010: tick <= 4'b0100;
+                4'b0100: tick <= 4'b1000;
+                4'b1000: tick <= 4'b0001;
+                default: tick <= 4'b0001;
+            endcase
+        end
     end
 endmodule
 
-
-module ALU (input_a, input_b, alu_op, result);
+module multiplexer(SignExtDin, R0, R1, R2, R3, R4, R5, R6, R7, G, sel, Bus);
 	/* 
-	 * This module implements the arithmetic logic unit of the processor.
+	 * This module takes 10 inputs and places the correct input onto the bus.
 	 */
-	// TODO: declare inputs and outputs
+	// TODO: Declare inputs and outputs
+	
+	// TODO: implement logic
 
 
-	// TODO: Implement ALU Logic:
 endmodule
 
+module ALU (input_a, input_b, alu_op, result);
+	 input [15:0] input_a;
+    input [15:0] input_b;
+    input [2:0] alu_op;
+    output reg [15:0] result;
+	 always @(*)begin 
+	 case (alu_op)
+            3'b000: result = input_a * input_b;
+            3'b001: result = input_a + input_b;
+            3'b010: result = input_a - input_b;
+            3'b011: result = $signed(input_b) >>> $signed(input_a);
 
+            default: result = 16'b0;
+        endcase
+    end
+	 
+endmodule
 
 module register_n(data_in, r_in, clk, Q, rst);
 
@@ -80,22 +74,14 @@ module register_n(data_in, r_in, clk, Q, rst);
 	// register_n #(.N(num_bits)) reg_IR(.....), 
 	// where num_bits is how many bits you want to set N to
 	// and "..." is your usual input/output signals
-	
+
 	parameter N = 16;
 
-
-	input wire [N-1:0] data_in;
-	input wire r_in;
-	input wire clk;
-	input wire rst;
-	output wire [N-1:0] Q;
+	/* 
+	 * This module implements registers that will be used in the processor.
+	 */
+	// TODO: Declare inputs, outputs, and parameter:
 	
-	always @(posedge clk) begin 
-		if (rst) begin
-		Q <= 'd0;
-		end 
-		else if (r_in) begin
-		Q <= data_in;
-		end
-	end
+	// TODO: Implement register logic:
 endmodule
+
